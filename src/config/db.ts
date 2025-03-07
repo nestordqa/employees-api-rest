f36d63e175143c9938064a30daf78c0104315c5a
Nestor Quinones <ndqa96@gmail.com>
2025-03-07 10:21:53 -0400

    feature: db connection already done

---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+//DB Conenction
+const connectDB = async (): Promise<void> => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI as string), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    } catch (error) {
+        console.error('There was an error trying to connect to the DB: ', error);
+        process.exit(1); // Exit from the API if it fails
+    }
+};
+
+export default connectDB;
